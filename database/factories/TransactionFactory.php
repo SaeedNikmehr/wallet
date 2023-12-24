dfe1eafa031b8a396ca2463d22ee5a7b1fbb184b
saeed <hovalkafi@gmail.com>
2023-12-25 01:01:25 +0330

    refactor: minor improvement

--- database/factories/TransactionFactory.php
+++ database/factories/TransactionFactory.php
@@ -12,9 +12,9 @@ class TransactionFactory extends Factory
     public function definition(): array
     {
         return [
-            'user_id' => random_int(1, 10),
-            'amount' => random_int(1000, 1000000),
-            'reference_id' => random_int(1000, 1000000),
+            'user_id' => fake()->numberBetween(1, 10),
+            'amount' => fake()->numberBetween(1000, 1000000),
+            'reference_id' => fake()->numberBetween(1000, 1000000),
         ];
     }
 }
