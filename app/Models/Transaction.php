eb0b6a07dcd7a23cb84b78981aec2fef46b5a420
saeed <hovalkafi@gmail.com>
2023-12-24 19:34:01 +0330

    feat: user and transaction model

---
+++ app/Models/Transaction.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Transaction extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    protected function serializeDate(\DateTimeInterface $date): string
+    {
+        return $date->format('Y-m-d H:i:s');
+    }
+}
